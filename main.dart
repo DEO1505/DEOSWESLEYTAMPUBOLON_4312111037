64032a6d6e97424a082c5437aaceba7a0d48521f
DEO1505 <143806765+DEO1505@users.noreply.github.com>
2023-09-01 19:09:44 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -24,119 +24,58 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Text('Praktikum Minggu 2 Pemrograman Perangkat Bergerak')
+        title: Text('Deo Swesley Tampubolon_Praktikum Minggu 2 PBB')
       ),
       body: Center(
-      child: Row (
-      mainAxisAlignment: MainAxisAlignment.center,
-      children:<Widget>[
-      Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [ElevatedButton(
-      style: ElevatedButton.styleFrom(
-      primary: Colors.redAccent,
-      ),
-      child:Text ('Button 1',
-      style: TextStyle(
-      color: Colors.black),
-      ),
-      onPressed:() {},
-      ),
-      Container(width: 10,height: 10,),
-      ElevatedButton(
-      style: ElevatedButton.styleFrom(
-      primary: Colors.red,
-      ),
-      child:Text ('Button 2',
-      style: TextStyle(
-      color: Colors.black),),
-      onPressed:() {},
-      ),],
-      ),
-      Container(width: 10,height: 10,),
-      Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [ElevatedButton(
-      style: ElevatedButton.styleFrom(
-      primary: Colors.blueAccent,
-      ),
-      child:Text ('Button 3',
-      style: TextStyle(
-      color: Colors.black),
-      ),
-      onPressed:() {},
-      ),
-      Container(width: 10,height: 10,),
-      ElevatedButton(
-      style: ElevatedButton.styleFrom(
-      primary: Colors.lightBlue,
-      ),
-      child:Text ('Button 4',
-      style: TextStyle(
-      color: Colors.black),),
-      onPressed:() {},
-      ),],
-      ),
-      Container(width: 10,height: 10,),
-      Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [ElevatedButton(
-      style: ElevatedButton.styleFrom(
-      primary: Colors.greenAccent,
-      ),
-      child:Text ('Button 5',
-      style: TextStyle(
-      color: Colors.black),
-      ),
-      onPressed:() {},
-      ),
-      Container(width: 10,height: 10,),
-      ElevatedButton(
-      style: ElevatedButton.styleFrom(
-      primary: Colors.lightGreen,
-      ),
-      child:Text ('Button 6',
-      style: TextStyle(
-      color: Colors.black),),
-      onPressed:() {},
-      ),
-      ],
-      ),
-      Container(width: 10,height: 10,),
-      ],
-      ),
-      ),
-      );
-  }
-}
-
-class MyHomePage2 extends StatelessWidget {
-  const MyHomePage2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Praktikum Minggu 2 Pemrograman Perangkat Bergerak")),
-      body: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-      Container(child: Text("HELLO WORLD"),
-      color: Colors.red,
-      padding: EdgeInsets.all(20),
-      ),
-      Container(child: Text("HELLO WORLD"),
-      color: Colors.yellow,
-      padding: EdgeInsets.all(20),
-      ),
-      Container(child: Text("HELLO WORLD"),
-      color: Colors.green,
-      padding: EdgeInsets.all(20),
-      ),
-      Container(child: Text("HELLO WORLD"),
-      color: Colors.lightBlue,
-      padding: EdgeInsets.all(20),
-      ),
-      ],)
-    );
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.redAccent,
+              ),
+              child: Text ('Button 1',
+              style: TextStyle(
+                color: Colors.black),
+              ),
+              onPressed: () {},
+            ),
+            Container(width: 10, height: 10,),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.yellowAccent,
+              ),
+              child: Text ('Button 2',
+              style: TextStyle(
+                color: Colors.black),),
+                onPressed: () {},
+              ),
+              Container(width: 10, height: 10,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.greenAccent,
+                ),
+                child: Text ('Button 3',
+                style: TextStyle(
+                  color: Colors.black),
+                ),
+                onPressed: () {},
+              ),
+              Container(width: 10,height: 10,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blueAccent,
+                ),
+                child: Text ('Button 4',
+                style: TextStyle(
+                  color: Colors.black),
+                ),
+                onPressed: () {},
+              ),
+              Container(width: 10),
+          ],
+          ),
+          ),
+          );
   }
 }
